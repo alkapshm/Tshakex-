0b07b43fbc2bd0de15a93b42b602d3ca47de1e9c
TshAkETEAM <52387368+TshAkETEAM@users.noreply.github.com>
2020-04-21 01:57:21 +0300

    Update Tshake.lua

--- Tshake.lua
+++ Tshake.lua
@@ -1,11 +1,11 @@
 --[[
 
 --]]
-database = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
-serpent = dofile("./File_Libs/serpent.lua")
-JSON    = dofile("./File_Libs/dkjson.lua")
-json    = dofile("./File_Libs/JSON.lua")
-URL     = dofile("./File_Libs/url.lua")
+database = dofile("./Libs/redis.lua").connect("127.0.0.1", 6379)
+serpent = dofile("./Libs/serpent.lua")
+JSON    = dofile("./Libs/dkjson.lua")
+json    = dofile("./Libs/JSON.lua")
+URL     = dofile("./Libs/url.lua")
 http    = require("socket.http")
 https   = require("ssl.https")
 sudos   = dofile("sudo.lua")
@@ -5812,10 +5812,10 @@ end
 t = t..'"linkgroup":"'..link..'"}' or ''
 end
 t = t..'}}'
-local File = io.open('./File_Libs/'..bot_id..'.json', "w")
+local File = io.open('./Libs/'..bot_id..'.json', "w")
 File:write(t)
 File:close()
-sendDocument(msg.chat_id_, msg.id_,'./File_Libs/'..bot_id..'.json', '📮┇ عدد مجموعات التي في البوت { '..#list..'}')
+sendDocument(msg.chat_id_, msg.id_,'./Libs/'..bot_id..'.json', '📮┇ عدد مجموعات التي في البوت { '..#list..'}')
 end
 if text == 'المطور' or text == 'مطور' or text == 'المطورين' then
 local Text_Dev = database:get(bot_id..'Tshake:Text_Dev')
@@ -6768,10 +6768,10 @@ end
 t = t..'"linkgroup":"'..link..'"}' or ''
 end
 t = t..'}}'
-local File = io.open('./File_Libs/'..bot_id..'.json', "w")
+local File = io.open('./Libs/'..bot_id..'.json', "w")
 File:write(t)
 File:close()
-sendDocument(msg.chat_id_, msg.id_,'./File_Libs/'..bot_id..'.json', '📮┇ عدد مجموعات التي في البوت { '..#list..'}')
+sendDocument(msg.chat_id_, msg.id_,'./Libs/'..bot_id..'.json', '📮┇ عدد مجموعات التي في البوت { '..#list..'}')
 end
 if text == "تحديث السورس 📥" then
 send(msg.chat_id_,msg.id_,'☑┇تم التحديث')
